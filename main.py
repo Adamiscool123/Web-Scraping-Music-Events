b5e4798fdb320d825775b0ecf18a5e886c02e490
Adamiscool123 <adamidrissi177@gmail.com>
2025-04-28 00:10:35 +0100

    Add finishing touches

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import selectorlib
 from send_email import send_email
 import time
+import sqlite3
+
+connection = sqlite3.connect("data.db")
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
@@ -19,22 +22,30 @@ def extract(text):
 
 def store(extrac):
     """Store new data"""
-    with open("data.txt", "a") as file:
-        file.write(extrac + "\n")
+    row = extrac.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
-def read():
+def read(extrac):
     """Read data"""
-    with open("data.txt", "r") as file:
-        return file.read()
+    row  = extrac.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE Band = ? AND City = ? AND Date = ?", (band, city, date))
+    rows1 = cursor.fetchall()
+    return rows1
 
 if __name__ == '__main__':
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
-        content = read()
         if extracted != "No upcoming tours":
-            if not extracted in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey a new event was found!")
                 print("An email was sent!")
